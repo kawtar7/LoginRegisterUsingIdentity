9a55b947e3686a4d08b798ca5dee05557507fa58
kawtar7 <razzoukawtar7@gmail.com>
2024-01-04 18:54:34 -0800

    Add project files.

---
+++ CustomIdentity/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomIdentity.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage ="username invalid!!")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage = "password invalid!!")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [Display(Name ="remember me")]
+        public bool RememberMe { get; set;}
+    }
+}
---
+++ CustomIdentity/ViewModels/RegisterVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomIdentity.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType (DataType.Password)]
+        public  string? Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
